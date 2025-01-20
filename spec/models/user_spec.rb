65602dedeb2820ddfdb7116164b592b1ffa4b03b
apple <apple@Tottotto-MacBook-Air.local>
2025-01-21 01:32:10 +0900

    update:rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,12 +11,24 @@ RSpec.describe User, type: :model do
     expect(user).not_to be_valid
     expect(user.errors[:name]).to include("名前を入力してください")
   end
+  
+  it "メールアドレスが入力されているか" do
+    user = build(:user, email: nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("メールアドレスを入力してください")
+  end
 
-  it "is invalid with a duplicate email" do
+  it "メールアドレスがユニークか" do
     create(:user, email: "test@example.com")
     user = build(:user, email: "test@example.com")
     expect(user).not_to be_valid
     expect(user.errors[:email]).to include("メールアドレスはすでに存在します")
+  end
+
+  it "メールアドレスが有効な形式か" do
+    user = build(:user, email: "test")
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("有効なメールアドレスを入力してください")
   end
 
 end
