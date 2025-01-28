b981a21d258a7006c998ce922384fc4494df07a6
apple <apple@Tottotto-MacBook-Air.local>
2025-01-29 00:49:03 +0900

    update:login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :authenticate_user!
   allow_browser versions: :modern
-    # Turbo互換性のためのDevise設定
+  # Turbo互換性のためのDevise設定
   class TurboDeviseController < Devise::SessionsController
     def respond_to_on_destroy
       respond_to do |format|
