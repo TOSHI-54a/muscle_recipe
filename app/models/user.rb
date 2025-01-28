340ea9fe333e33b37aee8308549399c5fdc37153
apple <apple@Tottotto-MacBook-Air.local>
2025-01-29 01:36:05 +0900

    update:gender等

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
     validates :age, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 120 }, allow_nil: true
     validates :height, numericality: { only_integer: true, greater_than: 50, less_than_or_equal_to: 250 }, allow_nil: true
     validates :weight, numericality: { only_integer: true, greater_than: 10, less_than_or_equal_to: 150 }, allow_nil: true
+    validates :gender, inclusion: { in: %w(male female), message: "%(value) は無効です" }, allow_nil: true
 end
